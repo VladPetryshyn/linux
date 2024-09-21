217efadb7a7f48d6b9a275316c5fa995460876c6
vdick <vlad_petryshyn@pm.me>
2024-09-21 14:52:40 +0200

    syncing to the github

--- _nvim/lua/remap.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = ","
-
-vim.keymap.set("n", "<C-h>", vim.cmd.TmuxNavigateLeft)
-vim.keymap.set("n", "<C-j>", vim.cmd.TmuxNavigateDown)
-vim.keymap.set("n", "<C-k>", vim.cmd.TmuxNavigateUp)
-vim.keymap.set("n", "<C-l>", '<C-w>l')
-vim.keymap.set('n', 'R', ':tabnext<CR>')
-vim.keymap.set('n', 'E', ':tabprev<CR>')
-
